c224fe9538749856d12e497a535461e08bf8a64f
quyetnv001 <quyetnv@rikkeisoft.com>
2023-01-06 14:22:28 +0700

    dung khunf layout default

--- src/components/layouts/DefaultLayout/Sidebar/index.js
+++ src/components/layouts/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 
 const SideBar = () => {
-    return <div>side bar</div>;
+    return <aside className={cx('wrapper')}></aside>;
 };
 
 export default SideBar;
